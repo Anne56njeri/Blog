430a40577c43c5b8384dbc8f2fd86a82802dd4ac
MaryAnne45 <mary.jereh@gmail.com>
2018-04-18 11:13:54 +0300

    Created a view function for  index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
+from flask import render_template,request,redirect,url_for
 from . import main
+from flask_login import login_required
 
+# Views
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
-
+	title = 'Welcome to the personal blog app'
+	return render_template('index.html',title = title)
